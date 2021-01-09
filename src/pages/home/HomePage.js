3807c2e732035695f3bdaebbc16fe85b2e4b5307
Viski Paul <58706499+viskipaul@users.noreply.github.com>
2021-01-09 17:23:09 +0200

    [BUG FIX] Fix home page welome user name to be hidden if not set

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -2,10 +2,13 @@ import React, {useEffect} from "react";
 import homeImage from '../../assets/images/home.jpg';
 import {connect} from "react-redux";
 
+
+
 const HomePage = (user) => {
   return (
       <div className="home">
-          <div className="homeTitle">{`Hello, ${user.user.firstName} ${user.user.lastName}`}</div>
+          {user.user.firstname && user.user.lastname && <div className="homeTitle">{`Hello, ${user.user.firstName} ${user.user.lastName}`}</div>}
+          {!user.user.firstname && <div className="homeTitle">Hello!</div>}
           <div className="homeSubTitle">WELCOME TO AUTO PARK!</div>
           <img src={homeImage} className="homeImage"/>
       </div>
