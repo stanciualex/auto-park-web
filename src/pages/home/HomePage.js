a6f98e945f3aad88d5e7c6d28b45eeae0db27ee7
Viski Paul <58706499+viskipaul@users.noreply.github.com>
2021-01-09 17:26:39 +0200

    Fixed home page bug

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -7,8 +7,8 @@ import {connect} from "react-redux";
 const HomePage = (user) => {
   return (
       <div className="home">
-          {user.user.firstname && user.user.lastname && <div className="homeTitle">{`Hello, ${user.user.firstName} ${user.user.lastName}`}</div>}
-          {!user.user.firstname && <div className="homeTitle">Hello!</div>}
+          {user.user.firstName && user.user.lastName && <div className="homeTitle">{`Hello, ${user.user.firstName} ${user.user.lastName}`}</div>}
+          {!user.user.firstName && <div className="homeTitle">Hello!</div>}
           <div className="homeSubTitle">WELCOME TO AUTO PARK!</div>
           <img src={homeImage} className="homeImage"/>
       </div>
